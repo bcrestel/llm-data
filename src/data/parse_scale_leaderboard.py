b3edb3b570e147b62aacd795cabfa7b7dc36ccb8
Ben Crestel <ben.crestel@zoho.com>
2024-08-07 01:47:28 -0400

    [WIP] scraping and processing scale leaderboard

--- src/data/parse_scale_leaderboard.py
+++ src/data/parse_scale_leaderboard.py
@@ -2,32 +2,86 @@ import requests
 from bs4 import BeautifulSoup
 from io import StringIO
 import pandas as pd
+import re
+import pickle
+from pathlib import Path
+from datetime import datetime
+import logging
 
-from src.utils.constant import SCALE_LEADERBOARD_URL, SCALE_ADV_ROB
+from src.utils.constant import SCALE_LEADERBOARD_URL, SCALE_EVAL_ADV_ROB, SCALE_COL_MODEL, LOCAL_PATH_TO_RAW_DATA, SCALE_EVAL_MAPPING, LOCAL_PATH_TO_INT_DATA
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(filename)s--l.%(lineno)d: %(message)s",
+)
+logger = logging.getLogger(__name__)
+
 
 class ScaleLeaderbord:
     def __init__(self, url: str = SCALE_LEADERBOARD_URL) -> None:
         self.url = url
     
-    def scrape(self) -> None:
+    def get_raw_data(self) -> None:
+        """Scrape webpage and save html"""
         # scrape webpage
         response = requests.get(self.url)
         html_content = response.text
+        # serialize to data/raw
+        file_name = Path(LOCAL_PATH_TO_RAW_DATA) / self.file_name_raw()
+        with open(file_name, 'wb') as file:
+            pickle.dump(html_content, file)
+
+    @staticmethod
+    def file_name_raw() -> Path:
+        now = datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        return Path(f"scale_leaderboard_{date}_raw.pickle")
+
+    def parse_html(self) -> None:
+        # Load html
+        file_name = Path(LOCAL_PATH_TO_RAW_DATA) / self.file_name_raw()
+        with open(file_name, 'rb') as file:
+            html_content = pickle.load(file)
+        logger.info(f"Loaded file {file_name}")
+        # Extract tables
         soup = BeautifulSoup(html_content, 'html.parser')
-        # find all relevant leaderboard tables
-        tables = soup.find_all('div', class_="flex flex-col gap-4")
-        self.tables = {}
-        # parse all tables
-        for table in tables:
+        raw_tables = soup.find_all('div', class_="flex flex-col gap-4")
+        tables = []
+        # process all tables
+        for table in raw_tables:
+            # parse table
             table_html = str(table.find('table'))
             table_io = StringIO(table_html)
             table_pd = pd.read_html(table_io)[0]
 
             table_name = table.find('span').text
-            table_pd["leaderboard"] = table_name
-            self.tables[table_name] = table_pd
-        self.leaderbord_adv_rob = self.tables.pop(SCALE_ADV_ROB)
-        self.leaderbord_metrics = pd.concat(list(self.tables.values), ignore_index=True)
+            logger.info(f"Processing table {table_name}")
+            # process column Model
+            table_pd[SCALE_COL_MODEL] = table_pd[SCALE_COL_MODEL].apply(self.remove_leading_number)
+            # Rename score column
+            if table_name == SCALE_EVAL_ADV_ROB:
+                table_pd.rename(columns={'Number of Violations':'Score'}, inplace=True)
+            score_new_name= f"Score_{SCALE_EVAL_MAPPING[table_name]}"
+            table_pd.rename(columns={'Score':score_new_name}, inplace=True)
+            # Split 95% CI column
+            table_pd[[f"{score_new_name}_95CI_max", f"{score_new_name}_95CI_min"]] = table_pd["95% Confidence"].str.split('/', expand=True).astype(float).apply(lambda x: x + table_pd[score_new_name])
+            table_pd.drop(columns="95% Confidence", axis=1, inplace=True)
+            table_pd = table_pd[[SCALE_COL_MODEL, f"{score_new_name}_95CI_min", score_new_name, f"{score_new_name}_95CI_max"]]
+            logger.debug(table_pd)
+            tables.append(table_pd)
+        # Do an outer join of all tables
+        joined_table = tables.pop()
+        for table in tables:
+            joined_table = pd.merge(joined_table, table, on=SCALE_COL_MODEL, how="outer")
+        # TODO: Add a timestamp, based on raw file name
+        logger.debug(joined_table)
+        # TODO: Save table to intermediate folder
     
-    #def export_table(self) -> None:
-    # TODO
+    @staticmethod
+    def remove_leading_number(text):
+        return re.sub(r'^\d+(?:st|nd|rd)?', '', text)
+
+if __name__ == "__main__":
+    sc = ScaleLeaderbord()
+    #sc.get_raw_data()
+    sc.parse_html()
--- src/utils/constant.py
+++ src/utils/constant.py
@@ -1,4 +1,15 @@
+LOCAL_PATH_TO_RAW_DATA = "data/01_raw"
+LOCAL_PATH_TO_INT_DATA = "data/02_intermediate"
+
 PATH_TO_PRICING_IN_LLM_PRICING = "src/lib/data.ts"
 
 SCALE_LEADERBOARD_URL = "https://scale.com/leaderboard"
-SCALE_ADV_ROB = "Adversarial Robustness"
+SCALE_EVAL_ADV_ROB = "Adversarial Robustness"
+SCALE_COL_MODEL = "Model"
+SCALE_EVAL_MAPPING = {
+    "Coding": "Coding",
+    "Spanish": "Spanish",
+    "Instruction Following": "Instruct",
+    "Math": "Math",
+    "Adversarial Robustness": "Adversarial"
+}
